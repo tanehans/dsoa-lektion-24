00db38a7ba0d6f35b8cc4531e25d19b8ae42a14d
hansenjo <hansenjo@arcada.fi>
2024-01-08 11:37:33 +0200

    Lektion 1 Car.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+        System.out.println("Välkommen!");
+
+        Car car = new Car("Celica");
+        Car anotherCar = new Car("Veyron");
+
+        System.out.println(car.getName());
+        anotherCar.setName("Galardo");
+        System.out.println(anotherCar.getName());
     }
 }
